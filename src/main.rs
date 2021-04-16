1f80b0dcc5e90fd76924e595e1f12d95d9bf7800
suzhen <suzhen@guestin.cn>
2021-04-16 11:12:05 +0800

    add logo

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ fn main() {
     let mut settings = iced::Settings::default();
     settings.window.size = (300, 220);
     settings.window.resizable = false;
+    settings.window.icon = Some(iced::window::Icon::from_rgba(
+        include_bytes!("../icon.rgba")[..].into(),
+        94u32, 127u32).unwrap());
     settings.default_font = Some(include_bytes!("../fonts/pingfang.ttf"));
     ui::App::run(settings).unwrap();
 }
