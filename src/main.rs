74275a11b6fa9dc3dfcd07dce3d95a5e534abfaf
suzhen <suzhen@guestin.cn>
2021-04-19 11:56:47 +0800

    refactor:

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,14 @@ use iced::Application;
 mod ui;
 
 fn main() {
-    let mut settings = iced::Settings::default();
+    let mut settings: iced::Settings<()> = Default::default();
     settings.window.size = (300, 220);
     settings.window.resizable = false;
-    settings.window.icon = Some(iced::window::Icon::from_rgba(
+    settings.window.icon = iced::window::Icon::from_rgba(
         include_bytes!("../icon.rgba")[..].into(),
-        94u32, 127u32).unwrap());
+        94u32,
+        127u32)
+        .ok();
     settings.default_font = Some(include_bytes!("../fonts/pingfang.ttf"));
     ui::main_window::Form::run(settings).unwrap();
 }
